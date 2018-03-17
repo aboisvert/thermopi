b5a114a50a1bde025d2f08db5c47e058007a127c
Alex Boisvert <alex.boisvert@gmail.com>
2018-03-17 14:48:28 -0700

    Add temperature override + forceHvac
    Former-commit-id: c0ed8efdd2673ab0147fc7630644951bb4ff32be
    Former-commit-id: 5f9cafb0612d04f4b14ff7f1fbbf0964a57690c7

--- server/src/routes.nim
+++ server/src/routes.nim
@@ -1,5 +1,5 @@
 import rosencrantz, httpcore
-import parseutils, strutils, strtabs, times
+import parseutils, strutils, strtabs, times, options
 import db, tdata, tcontrol, temperature
 
 proc serializeSensorDataHuman(data: seq[SensorData]): string =
@@ -13,12 +13,51 @@ proc serializeSensorData(data: seq[SensorData]): string =
     result &= $s.instant & "\n"
     result &= $s.temperature & "\n"
 
+proc serializeCurrentState(sensorId: int): string =
+  result = ""
+  result &= serializeSensorData(getLatestSensorData(sensorId))
+
+  result &= $controlMode & "\n" # currentHvacMode
+  result &= $controlState.hvac & "\n" # currentHvacStatus
+
+  result &= $getLatestSensorData(mainSensorId)[0].temperature & "\n" # mainSensorTemperature
+  result &= $currentDesiredTemperature().toCelcius() & "\n" # desiredTemperature
+
+  let (upcomingPeriod, upcomingTime) = upcomingPeriod()
+  result &= $upcomingTime.toTime().toUnix() & "\n" # upcomingTime
+  result &= $upcomingPeriod.desiredTemperature.toCelcius() & "\n" # upcomingTemperature
+
+  if override.isSome:
+    result &= $options.get(override).temperature.toCelcius() & "\n" # overrideTemperature
+    result &= $options.get(override).until & "\n" # overrideUntil
+  else:
+    result &= "0\n" # overrideTemperature
+    result &= "0\n" # overrideUntil
+
+  if forceHvac.isSome:
+    result &= $options.get(forceHvac) & "\n"
+  else:
+    result &= "\n"
+
+proc serializeSensorData(sensorId: int, start: int64, `end`: int64, samples: int): string =
+  let raw = getSensorData(sensorId, start, `end`)
+  let normalized =
+    if raw.len < samples: raw
+    else: (
+      let step = ((`end` - start) div samples).int;
+      normalize(raw, start, `end`, step)
+    )
+  serializeSensorData(normalized)
+
 proc error(msg: string): Handler =
   complete(Http500, "Server Error: " & msg, {"Content-Type": "text/plain;charset=utf-8"}.newHttpHeaders)
 
 let gets = get[
   path("/")[
-    file("../web/src/thermopi.html")
+    if defined(debug):
+      file("../web/src/thermopi-debug.html")
+    else:
+      file("../web/src/thermopi.html")
   ] ~ pathChunk("/static")[
     dir("../web/src")
   ] ~ pathChunk("/nimcache")[
@@ -49,16 +88,7 @@ let gets = get[
           let start = if startParam == "": epochTime().int64 else: startParam.parseInt
           let `end` = if endParam == "": start - (60 * 60 * 24) else: endParam.parseInt # seconds, 24 hours by default
           let samples = if samplesParam == "": 1000 else: samplesParam.parseInt
-
-          let raw = getSensorData(sensorId, start, `end`)
-          let normalized =
-            if raw.len < samples: raw
-            else: (
-              let step = ((`end` - start) div samples).int;
-              normalize(raw, start, `end`, step)
-            )
-          let body = serializeSensorData(normalized)
-          return ok(body)
+          return ok(serializeSensorData(sensorId, start, `end`, samples))
         except:
           #let e = getCurrentException()
           let msg = getCurrentExceptionMsg()
@@ -66,21 +96,7 @@ let gets = get[
       )
     )
   ] ~ pathChunk("/api/current")[
-    intSegment(proc(sensorId: int): auto =
-      var body = ""
-      body &= serializeSensorData(getLatestSensorData(sensorId))
-
-      body &= $controlMode & "\n" # currentHvacMode
-      body &= $controlState.hvac & "\n" # currentHvacStatus
-
-      body &= $getLatestSensorData(mainSensorId)[0].temperature & "\n" # mainSensorTemperature
-      body &= $currentDesiredTemperature().toCelcius() & "\n" # desiredTemperature
-
-      let (upcomingPeriod, upcomingTime) = upcomingPeriod()
-      body &= $upcomingTime.toTime().toUnix() & "\n" # upcomingTime
-      body &= $upcomingPeriod.desiredTemperature.toCelcius & "\n"# upcomingTemperature
-      return ok(body)
-    )
+    intSegment(proc (sensorId: int): auto = ok(serializeCurrentState(sensorId)))
   ]
 ]
 
@@ -98,6 +114,33 @@ let posts = post[
       let rowid = insertSensorData(sensor, temperature)
       echo "rowid: " & $rowid
       ok("THXBYE - rowid " & $rowid)
+    )
+  ] ~ path("/api/forceHvac")[
+    body(proc(str: string): auto =
+      echo "POST api/forceHvac"
+      echo str
+      let lines = str.splitLines()
+      let newState = lines[0]
+      if newState   == "On":  forceHvac = some(On)
+      elif newState == "Off": forceHvac = some(Off)
+      else: forceHvac = none(HvacStatus)
+      ok("THXBYE " & $forceHvac)
+    )
+  ] ~ path("/api/override")[
+    body(proc(str: string): auto =
+      echo "POST api/override"
+      echo str
+      let lines = str.splitLines()
+      let overrideTemperature = lines[0].parseFloat
+      let overrideUntil = lines[1].parseInt
+      if overrideUntil != 0:
+        override = some(Override(
+          temperature: celcius(overrideTemperature),
+          until: overrideUntil
+          ))
+      else:
+        override = none(Override)
+      ok("THXBYE " & $override)
     )
   ]
 ]
--- web/src/temperature_units.nim
+++ web/src/temperature_units.nim
@@ -1,16 +1,22 @@
 import karax / jstrutils
-import math
+import math, times
 
 type
   TemperatureUnit* = enum
     Celcius, Fahrenheit
 
 proc celciusToFahrenheit*(c: float): float =
-  math.round(c * 1.8 + 32, 1)
+  math.round(c * 1.8 + 32, 2)
+
+proc fahrenheitToCelcius*(f: float): float =
+  math.round((f - 32) / 1.8, 2)
 
 proc format*(celcius: float, unit: TemperatureUnit): cstring =
   case unit
   of Celcius:
-    $celcius & "C"
+    $math.round(celcius, 1) & "C"
   of Fahrenheit:
-    $celciusToFahrenheit(celcius) & "F"
+    $math.round(celciusToFahrenheit(celcius), 1) & "F"
+
+proc at*(dt: DateTime, hour: int, minute: int, second: int): DateTime =
+  initDateTime(dt.monthday, dt.month, dt.year, hour, minute, second)
