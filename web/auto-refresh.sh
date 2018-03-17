11d4141eaff8632d797f2f51840a50045dfe9b12
Alex Boisvert <alex.boisvert@gmail.com>
2018-03-17 07:37:34 -0700

    Cosmetics
    Former-commit-id: 83be934648b5fd25a213283262f48bd39443460e
    Former-commit-id: 16dcd2ef7273dc9233638bc17eebe34d318fa2e9

--- web/auto-refresh.sh
+++ web/auto-refresh.sh
@@ -1,11 +1,22 @@
 #!/usr/local/bin/fish
 
+set DEFINES ""
+
+if test -n "$LOCAL"
+  set DEFINES $DEFINES -d:local
+end
+
+if test -n "$STUBS"
+  set DEFINES $DEFINES -d:stubs
+end
+
 while true
   echo "Waiting for changes ..."
   fswatch --one-event --recursive src
   #inotifywait --recursive --event modify ./ ; 
   echo "Compiling"
-  nim js src/thermopi_web.nim > nim-js-output.txt ^&1
+  echo nim js $DEFINES src/thermopi_web.nim
+  nim js $DEFINES src/thermopi_web.nim > nim-js-output.txt ^&1
 
   if test $status = 0
   	cp src/thermopi.html src/thermopi-debug.html
