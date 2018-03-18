fe3288e8eb35c5e6d9bfb84d8c9a7d2e99a1f7de
Alex Boisvert <alex.boisvert@gmail.com>
2018-03-18 10:07:21 -0700

    Catch exceptions in controlLoop()
    Former-commit-id: 88694abbf8f77346a2f26de69d88e0316d74893b
    Former-commit-id: bdf68f8783cae0be4ef87446b02c2f51c315dc0a

--- server/src/tcontrol.nim
+++ server/src/tcontrol.nim
@@ -241,18 +241,23 @@ proc controlLoop*(): void =
     var lastCheckpoint = 0.int64
 
     while true:
-      sleep(5 * 1000)
-      let now = epochTime().int64
+      try:
+        sleep(5 * 1000)
+        let now = epochTime().int64
 
-      # turn HVAC system on/off based on current temperature of main sensor
-      let sensorData = getLatestSensorData(mainSensorId)
-      if sensorData.len > 0:
-        let last = sensorData[0]
-        if last.instant > (now - 5 * 60):
-          let currentTemperature = celcius(last.temperature)
-          doControl(currentTemperature)
+        # turn HVAC system on/off based on current temperature of main sensor
+        let sensorData = getLatestSensorData(mainSensorId)
+        if sensorData.len > 0:
+          let last = sensorData[0]
+          if last.instant > (now - 5 * 60):
+            let currentTemperature = celcius(last.temperature)
+            doControl(currentTemperature)
 
-      # checkpoit database
-      if lastCheckpoint < now - checkpointPeriod:
-        db.checkpoint()
-        lastCheckpoint = now
+        # checkpoit database
+        if lastCheckpoint < now - checkpointPeriod:
+          db.checkpoint()
+          lastCheckpoint = now
+      except:
+        let e = getCurrentException()
+        let msg = getCurrentExceptionMsg()
+        echo "controlLoop() exception ", repr(e), " with message ", msg
