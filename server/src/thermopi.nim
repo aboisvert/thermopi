8e8f9b72728d11c5ab6dbf85ca807707e3c25ad3
Alex Boisvert <alex.boisvert@gmail.com>
2018-03-20 20:27:24 -0700

    Catch exceptions in server.serve() to keep server up
    Former-commit-id: c476af47a3a9d526aabe6ef60b40818b9bee2090
    Former-commit-id: 21a48129fd6bc588f7996feef81de95d55e69248

--- server/src/thermopi.nim
+++ server/src/thermopi.nim
@@ -8,4 +8,13 @@ initTControl()
 spawn controlLoop()
 
 let server = newAsyncHttpServer()
-waitFor server.serve(Port(8080), handler)
+var clean = false
+while not clean:
+  try:
+    waitFor server.serve(Port(8080), handler)
+    clean = true
+  except:
+    let e = getCurrentException()
+    let msg = getCurrentExceptionMsg()
+    echo "server.serve() exception ", repr(e), " with message ", msg
+    
