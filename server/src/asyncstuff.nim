e4cd7699a6d008d972031d88626a06fb9fcbe775
Alex Boisvert <alex.boisvert@gmail.com>
2018-04-14 08:07:05 -0700

    Reduce verbosity
    Former-commit-id: 53e3073603920305a6b10f7ee1e48d92354c5399
    Former-commit-id: d9b5a586d809d143c8e026db7902e30ad994db47
    Former-commit-id: db437283a77952f41c16cd5afe70fe7b70f9e79a

--- server/src/asyncstuff.nim
+++ server/src/asyncstuff.nim
@@ -7,21 +7,21 @@ macro callAsync*(T: typedesc, input: T, U: typedesc, f: proc (t: T): U): untyped
   quote do:
     block:
       proc callp(evt: AsyncEvent, t: `T`): `U` =
-        echo "callp: " & $t
+        #echo "callp: " & $t
         let v = `f`(t)
-        echo "trigger: " & $v
+        #echo "trigger: " & $v
         evt.trigger()
         v
 
       proc async_call(input: `T`): Future[`U`] {.async.} =
         let evt = newAsyncEvent()
         let `fut` = callAsyncAwait(evt)
-        echo "before spawn: " & $input
+        #echo "before spawn: " & $input
         let val = spawn callp(evt, input)
-        echo "after spawn: " & $input
+        #echo "after spawn: " & $input
         `await` `fut`
         let v = ^val
-        echo "after await: " & $v
+        #echo "after await: " & $v
         return v
 
       async_call(`input`)
