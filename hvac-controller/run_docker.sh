a52a2320b3eddb8dd25cd8ed22511fb88666ad82
Alex Boisvert <alexb@ginger.io>
2021-01-31 13:43:51 -0800

    Run docker container as current $USER

--- hvac-controller/run_docker.sh
+++ hvac-controller/run_docker.sh
@@ -1,6 +1,31 @@
 #!/usr/bin/env fish
+
 if test -n "$USB"
   set USB_DEVICE "--device=/dev/ttyUSB0"
 end
 
-docker run --rm -v $PWD:/project $USB_DEVICE -w /project -it hvac_controller /usr/bin/fish
+set GID (id -g $USER)
+set UID (id -u $USER)
+
+set RUN_SCRIPT (mktemp -p $PWD)
+
+# Run container as current $USER to avoid messing up permissions 
+# when mounting $PWD to /project within the container
+
+echo "
+addgroup --gid $GID $USER
+mkdir -p /home/$USER
+useradd --home /home/$USER --gid $GID --uid $UID  $USER
+mkdir -p /home/$USER
+chown $USER:$GID /home/$USER
+chsh -s /usr/bin/fish $USER 
+runuser $USER
+" > $RUN_SCRIPT
+
+function on_exit --on-event fish_exit
+  #echo Deleting $RUN_SCRIPT
+  rm -rf $RUN_SCRIPT
+end
+
+set CONTAINER_SCRIPT /project/(basename $RUN_SCRIPT)
+docker run --rm -v $PWD:/project $USB_DEVICE -w /project -it hvac_controller /usr/bin/fish $CONTAINER_SCRIPT
